f8eb23948cd3efad28e1265d5693a24bc4dff147
Tes <tes66@yandex.ru>
2021-03-01 01:06:18 +0300

    lession 7 fixed

--- lession_7_dz_1.js
+++ lession_7_dz_1.js
@@ -5,14 +5,20 @@ const opentrash = document.querySelector('.opentrash');
   btn.addEventListener('click', function (e) {
     e.stopPropagation();
     modal.classList.add('active');
-    opentrash.display = 'block';
+    opentrash.style.display = 'block';
 
 
   })
 
-  document.addEventListener('click', function () {
-    modal.classList.add('active');
-    opentrash.display = 'none';
+  opentrash.addEventListener('click', function () {
+    modal.classList.remove('active');
+    opentrash.style.display = 'none';
 
   })
-  
+
+  document.addEventListener('keydown', function(e){
+    if(e.key == 'Escape') {
+      modal.classList.remove('active');
+      opentrash.style.display = 'none';
+    }
+  })

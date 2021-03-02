4bda30f8f0aa08ca3ae546b6ee1d54b575ff7e83
Tes <tes66@yandex.ru>
2021-03-02 21:50:44 +0300

    Lession 8 dz 1

--- lession_7_dz_1.js
+++ lession_7_dz_1.js
@@ -1,5 +1,7 @@
 const btn_1 = document.querySelector('.btn_1');
+const btn_2 = document.querySelector('.btn_2');
 const modal = document.querySelector('.modal');
+const modal_2 = document.querySelector('.modal_2');
 const opentrash = document.querySelector('.opentrash');
 
   btn_1.addEventListener('click', function (e) {
@@ -21,4 +23,19 @@ const opentrash = document.querySelector('.opentrash');
       modal.classList.remove('active');
       opentrash.style.display = 'none';
     }
+  })
+
+
+  btn_2.addEventListener('click', function (e) {
+    e.stopPropagation();
+    modal_2.classList.add('active');
+    opentrash.style.display = 'block';
+
+
+  })
+
+  opentrash.addEventListener('click', function () {
+    modal_2.classList.remove('active');
+    opentrash.style.display = 'none';
+
   })
